c0ac3e69c0e05e84b6ed6bc1044d29e2a803fb5c
Tom <51805114+MolniyaWaltz@users.noreply.github.com>
2022-03-28 15:04:42 +0100

    Add files via upload
    Units for rate-constants were off

--- o3decomp.m
+++ o3decomp.m
@@ -7,10 +7,10 @@ rho_AC = const.m_AC / const.V_AC;
 
 [mo3, ppm, po3, litres_o3] = o3gen(runtime);
 
-k1 = 50E-3 * mo3 / litres_o3;
-k2 = 2.13E-3 * mo3 / litres_o3; % k1, k2 const from literature are in m^3/g.s
+k1 = 50 * mo3 / litres_o3;
+k2 = 2.13 * mo3 / litres_o3; % k1, k2 const from literature are in m^3/g.s
 
-kd1 = 49.5E-6 * (mo3 / litres_o3) / const.M_o3; % kd1 in m^3/mol.s
+kd1 = 49.5E-3 * (mo3 / litres_o3) / const.M_o3; % kd1 in m^3/mol.s
 kd2 = 0.86E3 % kd2 in s^-1
 
 % diffusion coefficient calculations
